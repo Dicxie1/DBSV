0448b9bb92bfa45820977f1ddc6da01c0c6ac467
Dicxie Madrigal <hidannara@yahoo.es>
2021-10-06 19:20:13 -0600

    se ha eliminado el registro

--- Procedure/psInsertCategoria.sql
+++
@@ -1,7 +0,0 @@
-DELIMITER //
-CREATE PROCEDURE psInsertCategoria(IN parmIdCategoria INT,
-								   IN parmDescripcion VARCHAR(100))
-	BEGIN
-		INSERT INTO Categoria value(parmIdCategoria, parmDescripcion);
-    END//
-DELIMITER ;
