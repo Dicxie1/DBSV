b35d83cec88ce1301ba73b2e781feb25763c59b3
Dicxie Madrigal <hidannara@yahoo.es>
2021-11-18 06:31:54 -0600

    fix: cambio del tipo de datos de la columna userpassword a tipo BLOD

--- Tables/Usuario.sql
+++ Tables/Usuario.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Usuario(
 	idUsuario		INT PRIMARY KEY,
-    username		VARCHAR(15),
-    userpassword	VARCHAR(45),
+    username		VARCHAR(15) not null,
+    userpassword	BLOB not null,
     CONSTRAINT FK_Usuario_Empleado
     FOREIGN KEY (idusuario) REFERENCES Empleado(idempleado)
 );
